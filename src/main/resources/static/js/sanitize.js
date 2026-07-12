ef7a92cfa27cf38868f362f0a78dc1cc179c2753
prabhatkrmishra <mprabhat774@gmail.com>
2026-07-12 10:31:45 +0530

    Shoppiq: XSS protection via shared sanitize.js
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

---
+++ src/main/resources/static/js/sanitize.js
@@ -0,0 +1,12 @@
+/* Shared HTML/attribute escaping for XSS prevention */
+function escapeHtml(str) {
+    if (str == null) return '';
+    var d = document.createElement('div');
+    d.appendChild(document.createTextNode(String(str)));
+    return d.innerHTML;
+}
+
+function escapeAttr(str) {
+    if (str == null) return '';
+    return String(str).replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/'/g, '&#39;');
+}
