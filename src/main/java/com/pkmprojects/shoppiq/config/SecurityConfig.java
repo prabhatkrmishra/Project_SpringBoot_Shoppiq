46a3c10c58892c49b781b758062ccc35c8670566
prabhatkrmishra <mprabhat774@gmail.com>
2026-07-15 16:41:59 +0530

    Shoppiq: Update CSP configs with allowed origins
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- src/main/java/com/pkmprojects/shoppiq/config/SecurityConfig.java
+++ src/main/java/com/pkmprojects/shoppiq/config/SecurityConfig.java
@@ -362,7 +362,7 @@ public class SecurityConfig {
                                 .maxAgeInSeconds(31536000)  // 1 year
                         )
                         .contentSecurityPolicy(csp -> csp
-                                .policyDirectives("default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:")
+                                .policyDirectives("default-src 'self'; base-uri 'self'; form-action 'self'; frame-ancestors 'self'; connect-src 'self' https://unpkg.com https://cdn.jsdelivr.net https://cdn.quilljs.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com https://cdn.quilljs.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.quilljs.com https://cdn.jsdelivr.net; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:")
                         )
                 )
 
